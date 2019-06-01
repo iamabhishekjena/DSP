daa78b74b81dbd05bccb2d8446d1adf42db87073
Abhishek Jena <iamabhishekjena@gmail.com>
2019-06-01 09:54:57 +0530

    Add files via upload
    Number Plate Detection System

---
+++ Plate_detection.m
@@ -0,0 +1,28 @@
+close all;
+clear all;
+
+im = imread('Number Plate Images/car5.jpg');
+imgray = rgb2gray(im);
+imbin = imbinarize(imgray);
+im = edge(imgray, 'prewitt');
+
+%Below steps are to find location of number plate
+Iprops=regionprops(im,'BoundingBox','Area', 'Image');
+area = Iprops.Area;
+count = numel(Iprops);
+maxa= area;
+boundingBox = Iprops.BoundingBox;
+for i=1:count
+   if maxa<Iprops(i).Area
+       maxa=Iprops(i).Area;
+       boundingBox=Iprops(i).BoundingBox;
+   end
+end    
+
+im = imcrop(imbin, boundingBox);%crop the number plate area
+im = bwareaopen(~im, 500); %remove some object if it width is too long or too small than 500
+
+ [h, w] = size(im);%get width
+
+imshow(im);
+   
